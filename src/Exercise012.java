ff389ac05dd48897a06cbdce4a7f1c62ffeea8c6
manuelr <manuel_reyes@ultimatesoftware.com>
2022-09-25 22:18:40 -0400

    Adding exercises

---
+++ src/Exercise012.java
@@ -0,0 +1,24 @@
+// import Scanner from package java sub-package util;
+
+// declare a class named Exercise012
+	// declare your main method
+		// declare datatype Scanner variable called userInputReader, and assign a new Scanner with parameter System.in
+		// Print out line, literal string "What is your age?"
+		// declare datatype int variable called age
+		// assign variable age value from method call nextInt from userInputReader variable
+		// Print out line, literal String "You are ", concatenate with age variable, concatenate with literal String " years old"
+	// end main
+// end class
+
+
+/*
+A lot of new concepts here
+We are new-ing up a type of Scanner feeding into it the System input buffer.
+We assign that new Scanner to a variable called userInputReader.
+This essentially creates a tool for us to read user input in the console.
+Next we print a line asking for the user's age
+Next we declare a variable called age
+Next we assign the age variable the value returned from calling the method nextLine from the userInputReader variable
+This takes whatever the user inputted as a number, and everything to the right of the assignment operator (=) becomes the user input, another words the integer value age  
+Lastly we print out a line, printing the users input back to him
+*/
